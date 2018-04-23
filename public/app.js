425d437e2c75a1732d6e07a26664151d078a5246
Frederik Riedel <frederik.riedel@frogg.io>
2018-04-23 11:12:50 +0200

    This shows how to authenticate via Blockstack and store files.

---
+++ public/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('signin-button').addEventListener('click', function(event) {
+    event.preventDefault()
+    blockstack.redirectToSignIn()
+  })
+  document.getElementById('signout-button').addEventListener('click', function(event) {
+    event.preventDefault()
+    blockstack.signUserOut(window.location.href)
+  })
+
+  function showProfile(profile) {
+    var person = new blockstack.Person(profile)
+    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
+    if(person.avatarUrl()) {
+      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
+    }
+    document.getElementById('section-1').style.display = 'none'
+    document.getElementById('section-2').style.display = 'block'
+  }
+
+  if (blockstack.isUserSignedIn()) {
+      var profile = blockstack.loadUserData().profile
+      showProfile(profile)
+
+       blockstack.getFile("oasys.json").then((fileContents) => {
+
+          //document.getElementById('heading-name').innerHTML = fileContents
+
+          var d = new Date()
+          var n = d.getMilliseconds()
+          blockstack.putFile('oasys.json', "HELLO THIS IS A TEST" + n)
+       });
+      
+  } else if (blockstack.isSignInPending()) {
+    blockstack.handlePendingSignIn().then(function(userData) {
+      window.location = window.location.origin
+    })
+  }
+})
